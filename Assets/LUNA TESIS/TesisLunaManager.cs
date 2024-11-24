80014b3b587b6234f49f5df0d23a25ee6434767c
Diego Jimenez <diegojimenez2010@gmail.com>
2024-11-24 20:23:23 -0300

    Cambios en el pase al final

--- Assets/LUNA TESIS/TesisLunaManager.cs
+++ Assets/LUNA TESIS/TesisLunaManager.cs
@@ -284,7 +284,7 @@ public class TesisLunaManager : MonoBehaviour
                     cancionParte++;
                     Debug.Log("Esta sonando triste");
 
-                    if (cancionParte > clipsMimitosTristes.Count - 1)
+                    if (cancionParte == clipsMimitosTristes.Count - 1)
                     {
                         StartCoroutine(CambiarFase(duracionAudios - 1, Fases.final));
                         StartCoroutine(CambiarPuedeInteractuar(duracionAudios - 0.2f, Esculturas.ambas));
